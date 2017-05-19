87c9000e9fe95a41bad23ccf775b6c789f75c65a
ke <kejianok@126.com>
2017-05-19 15:13:51 +0800

    koa2-demo

---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Stephen on 17/5/19.
+ */
+"use strict"
+
+// const babel = require('babel-register');
+let koa = require('koa');
+let app = new koa();
+const convert = require('koa-convert');
+
+app.use(convert(function *(next){
+    var start = new Date;
+    yield next;
+    var ms = new Date - start;
+    this.set('X-Response-Time', ms + 'ms');
+    console.log(`X-Response-Time${ms}`);
+}));
+
+// logger
+
+app.use(convert(function *(next){
+    var start = new Date;
+    yield next;
+    var ms = new Date - start;
+    console.log('%s %s - %s', this.method, this.url, ms);
+}));
+
+// response
+
+app.use(convert(function *(){
+    this.body = 'Hello World';
+}));
+
+app.listen(8002);
+console.log('server run success in 8002');
