6ab2e899f3fd88714f286293f7c966b6d54fca96
ke <kejianok@126.com>
2017-05-23 18:59:24 +0800

    my vue2 demo

--- index.js
+++ index.js
@@ -5,35 +5,52 @@
 
 // const babel = require('babel-register');
 let koa = require('koa');
+let Router = require('koa-router');
+let mock = require('./mock_data');
 let app = new koa();
-let Mock = require('mockjs');
+let router = new Router();
 const convert = require('koa-convert');
 // let promise = require('bluebird');
 
-let data = Mock.mock('@email');
+// app.use(convert(function *(next){
+//     var start = new Date;
+//     yield next;
+//     var ms = new Date - start;
+//     this.set('X-Response-Time', ms + 'ms');
+//     console.log(`X-Response-Time${ms}`);
+// }));
+//
+// // logger
+//
+// app.use(convert(function *(next){
+//     var start = new Date;
+//     yield next;
+//     var ms = new Date - start;
+//     console.log('%s %s - %s', this.method, this.url, ms);
+// }));
 
-app.use(convert(function *(next){
-    var start = new Date;
-    yield next;
-    var ms = new Date - start;
-    this.set('X-Response-Time', ms + 'ms');
-    console.log(`X-Response-Time${ms}`);
-}));
+// app.use(convert(function *(){
+//     console.log(this.url)
+//
+// }));
+router
+    .get('/api/text', function (ctx, next) {
+        ctx.body = mock.text;
+    })
+    .get('/api/email', function (ctx, next) {
+        ctx.body = mock.email;
+    })
+    .get('/api/color', function (ctx, next) {
+        ctx.body = mock.color;
+    })
+    .get('/api/name', function (ctx, next) {
+        ctx.body = mock.name;
+    })
+    .get('/api/image', function (ctx, next) {
+        ctx.body = mock.image;
+    });
 
-// logger
-
-app.use(convert(function *(next){
-    var start = new Date;
-    yield next;
-    var ms = new Date - start;
-    console.log('%s %s - %s', this.method, this.url, ms);
-}));
-
-// response
-
-app.use(convert(function *(){
-    this.body = data;
-}));
+app.use(router.routes());
 
 app.listen(8002);
 console.log('server run success in 8002');
