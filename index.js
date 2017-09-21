5e0ef8434e46d013ea8a42dc24b88c01668fe51c
ke <kejianok@126.com>
2017-09-21 13:59:56 +0800

    add appHtml

--- index.js
+++ index.js
@@ -82,7 +82,10 @@ router
         let req = ctx.request.url;
         let reqPassword = req.slice(req.lastIndexOf('/') + 1);
         if (reqPassword == password) {
-          ctx.body = true;
+          // app.use(serve({rootDir: __dirname + '/app/build/static', maxage: 30 * 24 * 60 * 60 * 1000, gzip: true, rootPath: '/static'}));
+          let appPath = process.env.NODE_ENV === 'development'? '/app/public/index.html' : '/app/build/index.html';
+          let appHtml = fs.readFileSync(__dirname + appPath);
+          ctx.body = appHtml.toString();
         } else {
           ctx.body = false;
         }
@@ -90,5 +93,5 @@ router
 
 app.use(router.routes());
 
-app.listen(80);
-console.log('server run success in 80');
+app.listen(8002);
+console.log('server run success in 8002');
